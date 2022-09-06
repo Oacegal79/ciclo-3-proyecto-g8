677873d3b8a8842057d84972a9ac6749a3919c85
davidmintic <90147680+davidmintic@users.noreply.github.com>
2022-09-05 21:00:13 -0500

    cambios frontend razor

--- Taller.App/Taller.App.Front/Pages/Mecanico/GestionMecanico.cshtml.cs
+++ Taller.App/Taller.App.Front/Pages/Mecanico/GestionMecanico.cshtml.cs
@@ -5,8 +5,30 @@ namespace Taller.App.Front.Pages
 {
     public class GestionMecanicoModel : PageModel
     {
+
+        public string titulo { get; set; } = "El señor vera";
+
+        public List<Mecani> listaMecanicos = new List<Mecani>() {
+            new Mecani(){nombre = "David Rivera", especialidad = "Electricista", ciudad = "Barranquilla"},
+            new Mecani(){nombre = "David Pérez", especialidad = "Latonero", ciudad = "Manizales"}
+        };
+
         public void OnGet()
+        {
+            titulo = "El Señor Perez";
+            this.listaMecanicos.Add(new Mecani() { nombre = "David Pérez", especialidad = "Latonero", ciudad = "Manizales" });
+        }
+    }
+
+    public class Mecani
+    {
+        public string nombre { get; set; }
+        public string especialidad { get; set; }
+        public string ciudad { get; set; }
+
+        public Mecani()
         {
         }
     }
+
 }
