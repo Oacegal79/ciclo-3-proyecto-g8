5cc06e717e59169a705ba44f470b54a656208996
davidmintic <90147680+davidmintic@users.noreply.github.com>
2022-09-07 19:10:54 -0500

    gitignore

---
+++ Taller.App/Taller.App.Dominio/Entidades/Mecanico.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Taller.App.Dominio.Entidades
+{
+
+    public class Mecanico : Persona
+    {
+        public string nivelEstudio { get; set; }
+    }
+}
---
+++ Taller.App/Taller.App.Dominio/Entidades/Persona.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taller.App.Dominio.Entidades
+{
+    public class Persona
+    {
+        public string id { get; set; }
+        public string nombre { get; set; }
+        public string telefono { get; set; }
+        public string fechaNacimiento { get; set; }
+        public string contrasenia { get; set; }
+        public string rol { get; set; }
+
+
+    }
+}
